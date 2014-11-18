a0aff02325969f66fb9ffd3949f67855b3dd3d04
Cryptite <cryptite@gmail.com>
2014-11-18 09:22:59 -0600

    Auto-resizing player population

--- public/js/main.js
+++ public/js/main.js
@@ -11,16 +11,16 @@ $(function () {
     var announcement_message = $('.announcemessage');
     var announcement_avatar = $('.announceavatar');
 
+    var redContainer = $('.redplayers');
+    var blueContainer = $('.blueplayers');
+
+    /*Statistics Handling*/
     function updateStats(data) {
         var stats = $('.redstatsbody')
         var statsBlue = $('.bluestatsbody')
 
         stats.html('');
         statsBlue.html('');
-
-        for (var k in data.red) {
-            console.log(data.red[k]['score']);
-        }
 
         for (var red in data.red) {
             var redPlayer = data.red[red];
@@ -87,7 +87,125 @@ $(function () {
         }, 5000);
     }
 
-    //SOCKET SHIT
+    //TEST populating players
+    redContainer.html('');
+
+    setTimeout(function () {
+        populatePlayers({
+            red: {
+                0: {name: "Cryptite", score: 150, talents: [12, 24, 13]}},
+//                1: {name: "Magpieman", score: 100, "talents": [1, 2, 3]}},
+            blue: {
+                0: {name: "Cryptite", score: 150, talents: [12, 24, 13]}}
+//                1: {name: "Magpieman", score: 100, "talents": [1, 2, 3]}}
+        });
+    }, 1000);
+
+    /* Populate players */
+    function populatePlayers(data) {
+        redContainer.html('');
+        blueContainer.html('');
+
+        var counter = 200;
+        for (var red in data.red) {
+            var redPlayer = data.red[red];
+            addPlayer("red", redPlayer);
+
+            counter += 400;
+            $('.' + redPlayer['name']).velocity({
+                opacity: 1
+            }, {
+                duration: 500,
+                easing: 'easeOutQuart',
+                delay: counter});
+        }
+
+        updateContainerPosition("red", false);
+
+        for (var blue in data.blue) {
+            var bluePlayer = data.blue[blue];
+            addPlayer("blue", bluePlayer);
+
+            counter += 400;
+            $('.' + bluePlayer['name']).velocity({
+                opacity: 1
+            }, {
+                duration: 500,
+                easing: 'easeOutQuart',
+                delay: counter});
+        }
+
+        updateContainerPosition("blue", false);
+    }
+
+    //test player join
+//    setTimeout(function () {
+//        addPlayer("red", {name: "Cryptite", score: 150, talents: [12, 24, 13]});
+//        addPlayer("red", {name: "eevee500", score: 150, talents: [12, 24, 13]});
+//        updateContainerPosition("red");
+//
+//        addPlayer("blue", {name: "13lackpearl", score: 150, talents: [12, 24, 13]});
+//        addPlayer("blue", {name: "erodster", score: 150, talents: [12, 24, 13]});
+//        updateContainerPosition("blue");
+//    }, 6000);
+
+    function addPlayer(team, player) {
+        if (team == "red") {
+            var redHTML = '<div class="player ' + player['name'] + '"><p class="playername">'
+                + player['name'] + '</p><br><img src="./images/' + player['name']
+                + '.png" class="avatar"><br><p class="playerscore">' + player['score'] + '</p></div>';
+            redContainer.append(redHTML);
+        } else {
+
+            var blueHTML = '<div class="player ' + player['name'] + '"><p class="playername">'
+                + player['name'] + '</p><br><img src="./images/' + player['name']
+                + '.png" class="avatar"><br><p class="playerscore">' + player['score'] + '</p></div>';
+            blueContainer.append(blueHTML);
+        }
+
+        $('.' + player['name']).velocity({
+            opacity: 1
+        }, {
+            duration: 500,
+            easing: 'easeOutQuart',
+            delay: 500});
+    }
+
+    function updateContainerPosition(team, animated) {
+        if (team == "red") {
+            var count = redContainer[0].childNodes.length;
+
+            var newPos = 10 + (5 * count) + "%";
+            redContainer.css('left', newPos);
+
+//            if (animated) {
+//                redContainer.velocity({
+//                    left: newPos
+//                }, 500);
+//            } else {
+//                redContainer.css('left', newPos);
+//            }
+        } else {
+            var blueCount = redContainer[0].childNodes.length;
+
+            blueContainer.velocity({
+                right: 10 + (5 * blueCount) + '%'
+            }, 500);
+        }
+    }
+
+
+    //TEST updating player score
+    setTimeout(function () {
+        updatePlayerScore({name: "Cryptite", score: 200, "talents": [12, 24, 13]});
+    }, 4000);
+
+    function updatePlayerScore(data) {
+        var player = $('.' + data['name']);
+        player.find('.playerscore').html(data['score']);
+    }
+
+    /* Socket Work */
     var socket = io.connect('http://loka.minecraftarium.com:3001');
 
     socket.on('announce', function (data) {
