483e99a94288db93ee847d9f7b4a3d3cb01fefcc
Cryptite <cryptite@gmail.com>
2014-11-28 11:08:23 -0600

    No CD when on the same domain

--- app.js
+++ app.js
@@ -19,13 +19,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-};
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
