b7633be5957a55e4c15a4e96197be3ffc214b081
cryptite <crypt342>
2014-12-05 17:16:30 -0600

    Lots of things for tomorrow

--- public/js/dash.js
+++ public/js/dash.js
@@ -1,6 +1,5 @@
 $(function () {
-    var showStats = $('.showstats');
-    var hideStats = $('.hidestats');
+    var statsButton = $('.stats');
 
     var versusSubmit = $('.submitversus');
     var blueTeam = $('.blue');
@@ -9,19 +8,32 @@ $(function () {
     /* Socket Work */
     var socket = io.connect('http://loka.minecraftarium.com:3001');
 
-    showStats.click(function (evt) {
-        socket.emit("stats", {"stats": "show"});
+    statsButton.click(function (evt) {
+        if (statsButton.html() == "Show") {
+            socket.emit("stats", {"stats": "show"});
+            statsButton.html("Hide");
+        } else {
+            socket.emit("stats", {"stats": "hide"});
+            statsButton.html("Show");
+        }
         evt.preventDefault();
-    });
-
-    hideStats.click(function (evt) {
-        evt.preventDefault();
-        socket.emit("stats", {"stats": "hide"});
     });
 
     versusSubmit.click(function (evt) {
         evt.preventDefault();
-        console.log("submitting!");
         socket.emit("versus", {"blue": blueTeam.val(), "red": redTeam.val()});
+    });
+
+    setTimeout(function () {
+        setCurrentMatch({red: "Blobs", blue: "Pops"});
+    }, 2000);
+
+    function setCurrentMatch(data) {
+        blueTeam.val(data.blue);
+        redTeam.val(data.red);
+    }
+
+    socket.on('currentMatch', function (data) {
+        setCurrentMatch(data);
     });
 });
